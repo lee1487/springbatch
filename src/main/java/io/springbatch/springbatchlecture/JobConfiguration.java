1e4b5ab038d9263d82d6ec8e1df0449172bb6f34
hyeonse <dlgustp1487@naver.com>
2022-12-24 18:38:39 +0900

    JobRepository

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -2,6 +2,7 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -18,36 +19,33 @@ public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-    private final ExcutionContextTasklet1 excutionContextTasklet1;
-    private final ExcutionContextTasklet2 excutionContextTasklet2;
-    private final ExcutionContextTasklet3 excutionContextTasklet3;
-    private final ExcutionContextTasklet4 excutionContextTasklet4;
+    private final JobExecutionListener jobExecutionListener;
 
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("jobExcution")
+        return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
-                .next(step3())
-                .next(step4())
+                .listener(jobExecutionListener)
                 .build();
     }
 
     private Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(excutionContextTasklet1).build();
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
     }
 
     private Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(excutionContextTasklet2).build();
+                .tasklet((stepContribution, chunkContext) -> null)
+                .build();
     }
-    private Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(excutionContextTasklet3).build();
-    }
-    private Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet(excutionContextTasklet4).build();
-    }
+
+
 }
