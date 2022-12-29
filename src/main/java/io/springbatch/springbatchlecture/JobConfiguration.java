709d4a416fe9c9cb59c9a983f51dbf6d597fe721
hyeonse <dlgustp1487@naver.com>
2022-12-29 21:31:12 +0900

    JobBuilderFactory

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -21,30 +23,82 @@ public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+//    @Bean
+//    public Job batchJob1() {
+//        return jobBuilderFactory.get("batchJob1")
+//        		.incrementer(new RunIdIncrementer())
+//                .start(step1())
+//                .next(step2())
+//                .build();
+//    }
+
     @Bean
-    public Job job() {
-        return jobBuilderFactory.get("batchJob1")
+    public Job batchJob2() {
+        return jobBuilderFactory.get("batchJob2")
         		.incrementer(new RunIdIncrementer())
-                .start(step1())
-                .next(step2())
+                .start(flow())
+                .next(step5())
+                .end()
                 .build();
     }
 
-    private Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step1 was executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("step1 was executed");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                }).build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                	System.out.println("step2 was executed");
+//                	return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+
+    @Bean
+    public Flow flow() {
+    	FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+    	flowBuilder.start(step3())
+    				.next(step4())
+    				.end();
+
+    	return flowBuilder.build();
     }
 
-    private Step step2() {
-        return stepBuilderFactory.get("step2")
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
                 .tasklet((stepContribution, chunkContext) -> {
-                	System.out.println("step2 was executed");
+                	System.out.println("step3 was executed");
+                	return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((stepContribution, chunkContext) -> {
+                	System.out.println("step4 was executed");
+                	return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((stepContribution, chunkContext) -> {
+                	System.out.println("step5 was executed");
                 	return RepeatStatus.FINISHED;
                 })
                 .build();
